72e44dc0057a981a2c7dc563907c453487b7ff23
timtinan37 <timtinan@outlook.com>
2023-01-13 19:06:49 +0600

    wip: temp commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,25 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from "../components/layout"
 import { ThemeProvider } from 'next-themes'
+import { createContext, useState } from 'react'
+
+const CountryContext = createContext(null);
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [currentCountry, setCurrentCountry] = useState({});
+
   return (
     <ThemeProvider attribute="class">
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <CountryContext.Provider
+        value={{
+          currentCountry,
+          setCurrentCountry
+        }}
+      >
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </CountryContext.Provider>
     </ThemeProvider>
   )
 }
